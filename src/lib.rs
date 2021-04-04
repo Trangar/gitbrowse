7ed7d29dc67c6a5426414e6086cea600f3c07c92
Trangar <victor.koenders@gmail.com>
2021-04-04 12:29:58 +0200

    Added dependabot and build pipeline (#1)
    * Added dependabot and build pipeline
    * Fixed an issue where the main branch is not available on a pull request
    * Made sure the test doesn't fail when the commit is not found in CI
    * Made the test early return if no branches are found

--- src/lib.rs
+++ src/lib.rs
@@ -10,15 +10,27 @@
 //! # fn main() -> Result<(), Box<dyn std::error::Error>> {
 //!
 //! let repo = Repo::open(".")?;
-//! let branch = repo.browse_branch("main")?;
+//!
+//! let branches = repo.list_branches()?;
+//! println!("Found the following branches:");
+//! for branch in &branches {
+//!     println!(" - {}", branch);
+//! }
+//!
+//! # if branches.is_empty() {
+//! #     return Ok(());
+//! # }
+//!
+//! let branch = repo.browse_branch(branches.first().unwrap())?;
 //! for file in branch.list_files() {
 //!     println!("Found file: {:?}", file.path());
 //!     println!("File's content is length {}", file.read_content_string()?.len());
 //!     
 //!     println!("File is modified in the following commits:");
 //!     for commit in file.history()? {
-//!         let commit = commit?;
-//!         println!("  {}: {}", commit.id(), commit.message());
+//!         if let Ok(commit) = commit {
+//!             println!("  {}: {}", commit.id(), commit.message());
+//!         }
 //!     }
 //! }
 //!
